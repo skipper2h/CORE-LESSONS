90acc45352dc4ba335d84f7928feae1dce8aadd0
skipper2h <skipper2h@gmail.com>
2020-05-20 17:15:38 +0700

    Update 3.groovy

--- 3.groovy
+++ 3.groovy
@@ -5,17 +5,23 @@ Scanner in = new Scanner(System.in);
 System.out.print("input Poker Planning count ");
 String count = in.nextLine();
 in.close();
-boolean status = false;
-if(count.length() == 0) 
-System.out.print("String is empty ");
-break;
+boolean status 
+    
     for (int i = 0; i < array_string.length; i++) {
-        if (count == array_string[i]) {
-            status = true}
-        }    
-if (status == false) {
-    System.out.print("error")  }    
-else   
-if (status == true) {
-    System.out.print("poker")   }
+        if (count.length() == 0) 
+        {
+        System.out.print("String is empty ");
+        break;
+        }
+        else if (count == array_string[i]) 
+        {
+        System.out.print("poker")
+        status = false;
+        break;
+        }  
+}
+if (status = true)
+{
+System.out.print("error") 
+}
 }
