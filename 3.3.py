e4309697b005dfffd361baa68e6c530be289559d
skipper2h <skipper2h@gmail.com>
2020-05-29 11:08:11 +0700

    v.1.1

--- 3.3.py
+++ 3.3.py
@@ -1,20 +1,21 @@
 array_digit  = [0,0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100]
 array_string = ['coffee', '?']
-poker_number = str(input("Input "));
+poker_number = (input("Input "));
 #определение в массиве или нет
 def seachint():
     for i in array_digit:
-        if i == int(poker_number):
+        if i == int(poker_number) or i == float(poker_number):
             print ('poker');
-            break            
-        else:
-            print('nearest number is ', checktype()); 
-            break                   
+            break              
+        elif isinstance(poker_number, (int, float)) == True:  
+            print('nearest number is ', checknearest()); 
+            break
+        else: 
+            print ('input correct number');           
 #поиск ближайшего   
-def nearest(array, target):
- #return (array, key=lambda x: abs(x-target))  
+def nearest(array, target): 
    return array[min(range(len(array)), key = lambda i: abs(array[i]-target))]          
-def checktype():
+def checknearest():
     if poker_number.isdigit():    
         return ((nearest(array_digit , int(poker_number))));
     elif poker_number == array_string:
