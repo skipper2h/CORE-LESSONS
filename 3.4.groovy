0f71b71202db5a48f949087281846f7d89ec2b40
skipper2h <skipper2h@gmail.com>
2020-05-29 18:09:15 +0700

    fixed

--- 3.4.groovy
+++ 3.4.groovy
@@ -1,15 +1,29 @@
-i = 1
+//i ++: загрузить x из памяти, использовать, увеличивать, хранить обратно в память.
+// ++ i: загрузить x из памяти, увеличить, использовать, сохранить обратно в память.
+
 def post(){
+    int i = 1
     i = i++ 
-    println (i) //1
-    i = i++
-    println (i) //1
+    println (i) //1  
 }
 def pre(){
+    int i = 1
     i = ++i 
     println (i) //2
-    i = ++i
-    println (i) //3
+}
+def craftpost() {
+    int i = 1
+    temp = i
+    i = i + 1 
+    i = temp 
+    println (i) 
+}
+def crasftpre(){
+    int i = 1
+    i = i + 1
+    println (i) 
 }
 post()
 pre()
+craftpost() 
+crasftpre()
